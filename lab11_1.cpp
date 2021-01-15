0d79c33bfd7d33bb28733efaa2565f2f451899bc
thegenggo <kritsanaphong-keang@hotmail.com>
2021-01-15 15:37:23 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
+template <typename currentType>
+currentType fibonacci(currentType);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+template <typename currentType>
+currentType fibonacci(currentType x)
+{
+    if (x == 1 || x == 0) return x;
+    else if( x > 1)
+    {
+        return fibonacci(x - 1) + fibonacci(x - 2);
+    }
+    else return 0;
 }
